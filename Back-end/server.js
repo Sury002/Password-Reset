56d77c10770ee28d8bf2c578899b1fddeeba0eaf
Sury002 <suryabalaji791@gmail.com>
2025-05-19 16:45:19 +0530

    Updated

--- Back-end/server.js
+++ Back-end/server.js
@@ -4,6 +4,7 @@ const connectDB = require("./config/db");
 const cors = require("cors");
 
 const app = express();
+const sendEmail = require("./utils/sendEmail");
 
 
 app.use(cors({
@@ -26,6 +27,15 @@ app.get("/", (req, res) => {
 
 connectDB();
 app.use("/api/auth", require("./routes/authRoutes"));
-
 const PORT = process.env.PORT || 5000;
+
+app.get("/test-email", async (req, res) => {
+  try {
+    await sendEmail("your@gmail.com", "Test Email", "<h1>Test Email</h1>");
+    res.send("Email sent successfully!");
+  } catch (err) {
+    console.error("Email error:", err);
+    res.status(500).send("Failed to send email: " + err.message);
+  }
+});
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
