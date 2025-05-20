8ab46407b886a7cefcee32a83617c11371c7a6bb
Sury002 <suryabalaji791@gmail.com>
2025-05-20 10:41:07 +0530

    Udated

--- Back-end/server.js
+++ Back-end/server.js
@@ -20,7 +20,7 @@ const corsOptions = {
 };
 app.use(cors(corsOptions));
 
-app.options('*', cors(corsOptions)); // 👈 Add this line
+app.options('*', cors(corsOptions)); 
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
