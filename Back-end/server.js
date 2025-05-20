9ed7f77ee6721e529f509eb2da7e6547479a096b
Sury002 <suryabalaji791@gmail.com>
2025-05-20 10:24:14 +0530

    Updated

--- Back-end/server.js
+++ Back-end/server.js
@@ -13,12 +13,14 @@ const corsOptions = {
     "https://storied-sundae-fd4ddc.netlify.app",
     "http://localhost:5173"
   ],
-  methods: ["GET", "POST", "PUT", "DELETE"],
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization"],
   credentials: true,
   optionsSuccessStatus: 200 
 };
 app.use(cors(corsOptions));
 
+app.options('*', cors(corsOptions)); // 👈 Add this line
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
